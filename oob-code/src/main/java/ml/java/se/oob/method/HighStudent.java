2be378ede8cbb9cc8f930244619ef33aa098b3e8
jimeilin <jimeilin648@gmail.com>
2024-11-08 20:08:16 +0800

    instanceOf关键字判定变量是否属于指定类型以及子类型

--- oob-code/src/main/java/ml/java/se/oob/method/HighStudent.java
+++ oob-code/src/main/java/ml/java/se/oob/method/HighStudent.java
@@ -1,8 +1,21 @@
 package ml.java.se.oob.method;
 
-public class HighStudent extends Student{
+public class HighStudent extends Student {
+
+    // 向下转型判断
+    public static void demoInstanceOf() {
+        Student student = new Student();
+        System.out.println(student instanceof Student);
+        System.out.println(student instanceof HighStudent);
+
+        Student student1 = new HighStudent();
+        System.out.println(student1 instanceof Student);
+        System.out.println(student1 instanceof HighStudent);
+    }
     public static void main(String[] args) {
-        HighStudent highStudent = new HighStudent();
-        highStudent.printInterests(new String[]{});
+        // HighStudent highStudent = new HighStudent();
+        // highStudent.printInterests(new String[]{});
+
+        demoInstanceOf();
     }
 }
